88250361476f3b6842bad15416a8f35f756f6a84
Nikita <nlukjanov@gmail.com>
2017-12-04 07:05:05 +0000

    Implemented index view with posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,28 +1,41 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    user = User.create(
+                  email: "test@test.com",
+                  password: "asdasd",
+                  password_confirmation: "asdasd",
+                  first_name: "Nick",
+                  last_name: "Kang"
+                  )
+    login_as(user, :scope => :user)
+  end
+
   describe 'index' do
-    it 'can be reached succesfully' do
+    before do
       visit posts_path
+    end
+
+
+    it 'can be reached succesfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1")
+      post2 = Post.create(date: Date.today, rationale: "Post2")
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
   describe 'creation' do
     before do
-      user = User.create(
-                        email: "test@test.com",
-                        password: "asdasd",
-                        password_confirmation: "asdasd",
-                        first_name: "Nick",
-                        last_name: "Kang"
-                        )
-      login_as(user, :scope => :user)
       visit new_post_path
     end
 
