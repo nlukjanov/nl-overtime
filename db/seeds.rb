fc03da159c1356ef615f7d7b7abd02f63ffc3958
Nikita Lukjanov <nlukjanov@gmail.com>
2018-02-15 15:30:18 +0000

    Built out rake task for sending sms and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@user = User.create!(
+@employee = Employee.create!(
               email: "test@test.com",
               password: "asdasd",
               password_confirmation: "asdasd",
@@ -7,7 +7,7 @@
               phone: "5555555555"
               )
 
-puts '1 User created'
+puts '1 employee created'
 
 AdminUser.create!(
             email: "admin@test.com",
@@ -18,16 +18,16 @@ AdminUser.create!(
             phone: "5555555555"
             )
 
-puts '1 Admin User created'
+puts '1 Admin user created'
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts '3 audit logs have been created'
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content extended ext endede xtend edexten ded ext ended extendede xtended exten ded extend dext ended exten dedext endedext ended extend edexten ded extended  extended extended extended extended extended v extended  sadfds asdf asdf asdfas dfas dfas dfas dfasd fasdf asdf asd fasd fas df asdf das fs adf asd fas df asd fas df asd fasd ", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content extended ext endede xtend edexten ded ext ended extendede xtended exten ded extend dext ended exten dedext endedext ended extend edexten ded extended  extended extended extended extended extended v extended  sadfds asdf asdf asdfas dfas dfas dfas dfasd fasdf asdf asd fasd fas df asdf das fs adf asd fas df asd fas df asd fasd ", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts '100 posts have been created'
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -2,23 +2,16 @@ namespace :notification do
   desc "Send SMS notification to employees asking to log if they had overtime or not"
   task sms: :environment do
     if Time.now.sunday?
-      # 1. Schedule to run at Sunday at 5pm
-      # 2. Iterate over all employees
-      # 3. Skip admin users
-      # 4. Send a message that has instructions and a link to log time
-      # User.all.each do |user|
-      #   SmsTool.send_sms()
-      # end
+      employees = Employee.all
+      notification_message = "Please log into the overtime management dashboard to request overtime or confirm your hours for last week: https://nl-overtime.herokuapp.com/"
+      employees.each do |employee|
+        SmsTool.send_sms(number: employee.phone, message: notification_message)
+      end
     end
   end
 
   desc "Sends mail notification to managers (admin users) each day to inform of pending overtime requests"
   task manager_email: :environment do
-    puts "I'm in the manager email"
-    # 1. Iterate over list of pending overtime requests
-    # 2. Check to see if there are any requests
-    # 3. Iterate over the list of admin users/managers
-    # 4. Send email to each admin
     submitted_posts = Post.submitted
     admin_users = AdminUser.all
 
