3feda3560c2c8769f708342d3227d043e2e891f7
Nikita Lukjanov <nlukjanov@gmail.com>
2018-02-15 15:59:03 +0000

    Added mailer config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,5 +23,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.serve_static_assets = true
   config.assets.digest = true
+  config.action_mailer.default_url_options = { host: 'nl-overtime.herokuapp.com' }
 
 end
